6d6243aa3f4dca7dd22f8c4914cb68461ff4e637
yunCrush <yunfei1577266641@gmail.com>
2022-04-10 00:26:25 +0800

    基本类型

---
+++ src/main/java/com/yun/redis/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.yun.redis.controller;
+
+/**
+ * Author: yunCrush
+ * Date:2022/4/9 21:17
+ * Description:
+ */
+public class ArticleController {
+}
---
+++ src/main/java/com/yun/redis/service/ArticleService.java
@@ -0,0 +1,9 @@
+package com.yun.redis.service;
+
+/**
+ * Author: yunCrush
+ * Date:2022/4/9 21:18
+ * Description:
+ */
+public class ArticleService {
+}
